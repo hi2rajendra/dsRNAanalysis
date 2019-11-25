8ca885a7d19f5ce65de6d02f9e32534693c2dfd3
Rajendra K. Labala <50165429+hi2rajendra@users.noreply.github.com>
2019-11-25 17:01:24 +0530

    Update remove_non_atgc.pl

--- remove_non_atgc.pl
+++ remove_non_atgc.pl
@@ -8,13 +8,10 @@ my $AUTHOR = "Rajendra K. Labala"; my $DATE1 = "Jan/2015";
 use strict;
 use warnings;
 use Bio::SeqIO;
-
 my $in = Bio::SeqIO->new( -file => shift, -format => 'Fasta' );
-
 while ( my $seq = $in->next_seq() ) {
     print '>' . $seq->id . ' ' . $seq->desc . "\n" . $seq->seq . "\n"
       if $seq->seq !~ /[qweryiopsdfhjklzxvbnm1234567890]/i;
 }
-
 print "\n*********************************END**********************************\n";
 print "\nScript Details\n===============\nAuthor: $AUTHOR\nVersion: $VERSION\nDate Created: $DATE1\n\n";
