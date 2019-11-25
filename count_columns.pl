09031b5ddad8f510c3cd8f4d1972c0a70631eca5
Rajendra K. Labala <50165429+hi2rajendra@users.noreply.github.com>
2019-11-25 17:06:36 +0530

    Update count_columns.pl

--- count_columns.pl
+++ count_columns.pl
@@ -7,12 +7,9 @@ my $VERSION = "1.0 [tab delimeted file]";
 my $AUTHOR = "Rajendra K. Labala"; my $DATE1 = "Aug/2017";
 ###############################################START###################################RAJ###############
 use strict;
-
 my $input_file=$ARGV[0];
 open(IN,"<$input_file") || die ("Error opening $input_file $!");
-
 my @lines=<IN>;
-
 my $line;
 my $count;
 #print "$line";
