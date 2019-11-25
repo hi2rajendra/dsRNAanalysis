1e70b5f5771df5faa7ee24173abe5bc403baa695
Rajendra K. Labala <50165429+hi2rajendra@users.noreply.github.com>
2019-11-25 16:31:22 +0530

    Update matrix_sorting.pl

--- matrix_sorting.pl
+++ matrix_sorting.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
-#Description:Program to extract both the lines containing the given string and without the string and create separate files. (cut and paste kind)
+#Description:Program to extract both the lines containing the given string and without the string and 
+#create separate files. (cut and paste kind)
 #to find RAs in 
 #1. present in one species (new file 1 will be created)
 #2. present in all species (new file 2 will be created)
@@ -11,15 +12,14 @@ my $AUTHOR = "Rajendra K. Labala"; my $DATE1 = "July/2017";
 ###############################################START###################################RAJ################
 use strict; 
 use warnings; 
-
 print "\n********************************START********************************\n";
-
 open( FILE, "<Match_Matrix_RA_only_re-sorting.txt" ); #opening the parent file
 my @LINES = <FILE>; 
 close( FILE ); 
-open (NEW, ">", "Match_Matrix_RA_only_re-sorting_hits_only_in_one_species.txt" ) or die "could not open:$!"; #creating new file containing the string
+open (NEW, ">", "Match_Matrix_RA_only_re-sorting_hits_only_in_one_species.txt" ) or die "could not open:$!"; 
+#creating new file containing the string
 open( FILE, ">Match_Matrix_RA_only_re-sorting.txt" ) or die "could not open:$!"; #give a new name if you want to
-    #cteate a new file containing the rest of the lines, OR give the same parent file name to delete inside it
+#cteate a new file containing the rest of the lines, OR give the same parent file name to delete inside it
 foreach my $LINE ( @LINES ) {
         print NEW $LINE if ($LINE =~ m/;[0-9]*.[0-9]*;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA/ ||
                             $LINE =~ m/NA;[0-9]*.[0-9]*;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA/ ||
@@ -46,28 +46,22 @@ foreach my $LINE ( @LINES ) {
                             $LINE =~ m/NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;[0-9]*.[0-9]*;NA/ ||
                             $LINE =~ m/NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;[0-9]*.[0-9]*/);  #printing new file without the string
     } 
-   
-    close( NEW );
-    close( FILE );
-    print( "\nsuccessfully created files 'with' and 'without' the string\n" );
-    
-    
-    open( FILE2, "<Match_Matrix_RA_only_re-sorting.txt" ); #opening the parent file
-    my @LINES2 = <FILE2>; 
-    close( FILE2 ); 
-    
-    open (NEW2, ">", "Match_Matrix_RA_only_re-sorting_hits_in_all_species.txt" ) or die "could not open:$!"; #creating new file containing the string
-    open( FILE2, ">Match_Matrix_RA_only_re-sorting.txt" ) or die "could not open:$!"; #give a new name if you want to
-    #cteate a new file containing the rest of the lines, OR give the same parent file name to delete inside it
-        
-    foreach my $LINE2 ( @LINES2 ) {
-        print NEW2 $LINE2 if ($LINE2 =~ m/;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*/); 
-        print FILE2 $LINE2 unless ( $LINE2 =~ m/;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*/);  
-        #printing new file without the string
-    } 
-    close( NEW2 );
-    close( FILE2 );
-    print( "\nsuccessfully created files 'with' and 'without' the string\n" ); 
-
+close( NEW );
+close( FILE );
+print( "\nsuccessfully created files 'with' and 'without' the string\n" );
+open( FILE2, "<Match_Matrix_RA_only_re-sorting.txt" ); #opening the parent file
+my @LINES2 = <FILE2>; 
+close( FILE2 ); 
+open (NEW2, ">", "Match_Matrix_RA_only_re-sorting_hits_in_all_species.txt" ) or die "could not open:$!"; #creating new file containing the string
+open( FILE2, ">Match_Matrix_RA_only_re-sorting.txt" ) or die "could not open:$!"; #give a new name if you want to
+#cteate a new file containing the rest of the lines, OR give the same parent file name to delete inside it
+foreach my $LINE2 ( @LINES2 ) {
+    print NEW2 $LINE2 if ($LINE2 =~ m/;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*/); 
+    print FILE2 $LINE2 unless ( $LINE2 =~ m/;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*;[0-9]*.[0-9]*/);  
+    #printing new file without the string
+} 
+close( NEW2 );
+close( FILE2 );
+print( "\nsuccessfully created files 'with' and 'without' the string\n" ); 
 print "\n*********************************END*********************************\n";
 print "\nScript Details\n===============\nAuthor: $AUTHOR\nVersion: $VERSION\nDate Created: $DATE1\n\n";
